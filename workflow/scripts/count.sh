f5c88e5f6cbbd13b9779a539ac1fbdb615df31ed
niekwit <41302859+niekwit@users.noreply.github.com>
2024-06-06 14:41:51 +0100

    replaced with shell directive

--- workflow/scripts/count.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-
-zcat ${snakemake_input[fq]} | hisat2 --no-hd -p ${snakemake[threads]} -t -N ${snakemake_params[mm]} -x ${snakemake_params[idx]} - 2> ${snakemake_log[0]} | sed '/XS:/d' | cut -f3 | sort | uniq -c | sed 's/^ *//' | sed '1d' > ${snakemake_output[0]}
