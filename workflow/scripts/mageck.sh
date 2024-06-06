1e636f3361ae5a2e7a019eb8d1ff0a899797f082
niekwit <41302859+niekwit@users.noreply.github.com>
2024-06-06 11:26:18 +0100

    replaced with Python script

--- workflow/scripts/mageck.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-mageck test --normcounts-to-file -k ${snakemake_input[0]} -t $(echo ${snakemake_wildcards[mcomparison]} | awk -F '_vs_' '{print $1}' | sed 's/-/,/') -c $(echo ${snakemake_wildcards[mcomparison]} | awk -F '_vs_' '{print $2}' | sed 's/-/,/') -n results/mageck/${snakemake_wildcards[mcomparison]}/${snakemake_wildcards[mcomparison]} ${snakemake_params[control]} ${snakemake_params[extra]} 2> ${snakemake_log[0]}
