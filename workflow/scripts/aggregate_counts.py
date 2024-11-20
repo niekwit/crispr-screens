a96a6f5cd8d19462acc0c29c7237e6f1accd2c25
niekwit <41302859+niekwit@users.noreply.github.com>
2024-11-20 08:09:20 +0000

    simplified

--- workflow/scripts/aggregate_counts.py
+++ workflow/scripts/aggregate_counts.py
@@ -3,46 +3,34 @@ import pandas as pd
 
 ''' 
 Join count files to create count table
+
+sgRNA names are expected to be in the format:
+GENE_sgGENE_1, etc
 '''
 
-log = snakemake.log_fmt_shell(stdout=True, stderr=True)
-
-counts = {}
-    
-for file in snakemake.input:
-    # Check if file size is larger than 0
-    assert os.path.getsize(file) > 0, f"{file} is empty (alignment failed?)"
-    
-    # Add counts to counts dict
-    df = pd.read_csv(file,sep=" ",header = None)
-    key = os.path.basename(file).replace(".guidecounts.txt", "")
-    df.columns = [key, "sgRNA"]
-    df[key].astype(int)
-    counts.update({key:df})
-
-df = pd.read_csv(snakemake.params[0], header = None)
+# Prepare data frame with gene and sgRNA names
+fasta = pd.read_csv(snakemake.input["fasta"], header=None)
+df = fasta[fasta[0].str.contains(">")].copy()
+df.loc[:,0] = fasta.loc[:,0].str.replace(">", "", regex=False)
 df.columns = ["sgRNA"]
+df.loc[:,"gene"] = df.loc[:,"sgRNA"].str.split(pat="_", n=1, expand=True)[0]
 
-df = df[df["sgRNA"].str.contains(">")]
-df = df.reset_index(drop = True)
-df["sgRNA"] = df["sgRNA"].str.replace(">", "")
-df["gene"] = df["sgRNA"].str.split(pat = "_",n = 1,expand = True)[0]
+# Merge all count files to df to create count table
+for file in snakemake.input["files"]:
+    # Read count file
+    tmp = pd.read_csv(file, sep=" ", header=None)
+    
+    # Rename headers to wildcard value (for counts) and sgRNA 
+    wildcard_value = os.path.basename(file).replace(".guidecounts.txt", "")
+    tmp.columns = [wildcard_value, "sgRNA"]
+    
+    # Merge to df
+    df = pd.merge(df, tmp, on="sgRNA", how="left")
 
-# Perform left join on count files
-for key, value in counts.items():
-    df = pd.merge(df, value, on='sgRNA', how='left')
-df["sgRNA"] = df["sgRNA"].str.split(pat = "_",n = 1,expand = True)[1]
-
-# Replace nan with zero
+# Replace missing values with zero and round to whole numbers
 df = df.fillna(0)
-df = df.sort_values(by = ["sgRNA"])
-
-# Convert floats back to int after pandas merge (bug in pandas)
-index_range = range(2, len(df.columns))
-index_list = []
-for i in index_range:
-    index_list.append(i)
-df[df.columns[index_list]] = df[df.columns[index_list]].astype(int)
+numeric_cols = df.select_dtypes(include="number").columns
+df[numeric_cols] = df[numeric_cols].round(decimals=0).astype(int)
 
 # Save data frame to file
-df.to_csv(snakemake.output[0], sep = '\t', index = False)
+df.to_csv(snakemake.output[0], sep='\t', index=False)
