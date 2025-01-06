32ef2122ab8bd331e6436d2a52a6a847792ecb66
niekwit <41302859+niekwit@users.noreply.github.com>
2025-01-06 16:06:59 +0000

    fetch info from csv and not fasta

--- workflow/scripts/aggregate_counts.py
+++ workflow/scripts/aggregate_counts.py
@@ -3,22 +3,23 @@ import pandas as pd
 
 ''' 
 Join count files to create count table
-
-sgRNA names are expected to be in the format:
-GENE_sgGENE_1, etc
 '''
 
 # Prepare data frame with gene and sgRNA names
-fasta = pd.read_csv(snakemake.input["fasta"], header=None)
-df = fasta[fasta[0].str.contains(">")].copy()
-df.loc[:,0] = fasta.loc[:,0].str.replace(">", "", regex=False)
-df.columns = ["sgRNA"]
-df.loc[:,"gene"] = df.loc[:,"sgRNA"].str.split(pat="_", n=1, expand=True)[0]
+gene_column = snakemake.config["csv"]["gene_column"]
+name_column = snakemake.config["csv"]["name_column"]
+csv = pd.read_csv(snakemake.input["csv"])
+df = csv.iloc[:, [name_column, gene_column]]
+df.columns = ["sgRNA", "gene"]
 
 # Merge all count files to df to create count table
 for file in snakemake.input["files"]:
     # Read count file
-    tmp = pd.read_csv(file, sep=" ", header=None)
+    try:
+        tmp = pd.read_csv(file, sep=" ", header=None)
+    except pd.errors.EmptyDataError:
+        # Create empty data frame if file is empty
+        tmp = pd.DataFrame(columns=[0, 1])
     
     # Rename headers to wildcard value (for counts) and sgRNA 
     wildcard_value = os.path.basename(file).replace(".guidecounts.txt", "")
@@ -27,12 +28,19 @@ for file in snakemake.input["files"]:
     # Merge to df
     df = pd.merge(df, tmp, on="sgRNA", how="left")
 
+# Check if columns other than 'gene' and 'sgRNA' contain only NAs
+count_columns = df.columns.difference(["gene", "sgRNA"])
+na_columns = df[count_columns].isna().all()
+
+if na_columns.all():
+    raise ValueError("All count columns are empty. Check trim/alignment settings.")
+
 # Replace missing values with zero and round to whole numbers
 df = df.fillna(0)
 numeric_cols = df.select_dtypes(include="number").columns
 df[numeric_cols] = df[numeric_cols].round(decimals=0).astype(int)
 
-# Remove duplicate rows (observed in some cases)
+# Remove duplicate rows (observed for some libraries)
 df = df.drop_duplicates()
 
 # Save data frame to file
