567023cb50f15903af81ea8f198917ddbe375b97
niekwit <41302859+niekwit@users.noreply.github.com>
2025-01-10 14:08:52 +0000

    fixed if statement

--- workflow/scripts/csv_to_fasta.py
+++ workflow/scripts/csv_to_fasta.py
@@ -5,20 +5,20 @@ Convert csv file to fasta file. Barcode name and sequence
 column numbers must be specified in config.yml.
 """
 
-barcode_column = snakemake.config["csv"]["name_column"]
+sgrna_column = snakemake.config["csv"]["name_column"]
 sequence_column = snakemake.config["csv"]["sequence_column"]
 
 # Load csv file
 df = pd.read_csv(snakemake.input["csv"])
 
-# Only keep barcode ID and sequence columns
-df = df.iloc[:, [barcode_column, sequence_column]]
+# Only keep sgRNA ID and sequence columns
+df = df.iloc[:, [sgrna_column, sequence_column]]
 
 # Check if sequence column is a DNA sequence
-if not df.iloc[:, 1].str.match(r"^[ACGTacgt]+$").all():
-    raise ValueError("Sequence column must contain DNA sequences.")
+if not df.iloc[:, 1].str.match(r"^[ACGTacgt]+$").any():
+    raise ValueError("Sequence column must contain valid DNA sequences.")
 
-# Prepend ">" to the barcode ID
+# Prepend ">" to the sgRNA ID
 df.iloc[:, 0] = ">" + df.iloc[:, 0]
 
 # Write fasta file
